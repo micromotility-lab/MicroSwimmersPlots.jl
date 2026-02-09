af7524bfe89c0dcdb15bb51c4cdd3914b9db12d5
James Cass <jamesfcass@gmail.com>
2026-02-09 13:39:26 +0000

    local changes

--- src/viz.jl
+++ src/viz.jl
@@ -206,11 +206,10 @@ end
 #     U, V
 # end
 
-function viz(vf::PlanarVelocityField)
+function viz(vf::PlanarVelocityField; fig = Figure())
     points2 = [_proj2(vf.plane, x) for x in vf.points]
     vels2   = [_proj2(vf.plane, v) for v in vf.velocities]
 
-    fig = Figure()
     (xl, yl) = _labels(vf.plane)
     ax = Axis(fig[1,1], aspect=DataAspect(), xlabel=xl, ylabel=yl)
 
@@ -225,7 +224,7 @@ function viz(vf::PlanarVelocityField)
     fig
 end
 
-function stream(vf::PlanarVelocityField; colorscale=identity)
+function stream(vf::PlanarVelocityField; fig = Figure(), colorscale=identity)
     @unpack plane, a_range, b_range, c, velocities = vf
     ind = plane === :xy ? (1,2) : (plane === :xz ? (1,3) : (2,3))
     n1, n2 = length(a_range), length(b_range)
@@ -239,7 +238,6 @@ function stream(vf::PlanarVelocityField; colorscale=identity)
     u_func(x, y) = Point2(itp_u(x, y), itp_v(x, y))
 
     vel_mag = sqrt.(U.^2 .+ V.^2)
-    fig = Figure()
     (xl, yl) = _labels(plane)
     ax = Axis(fig[1,1], xlabel=xl, ylabel=yl, aspect=DataAspect())
     hm = heatmap!(ax, a_range, b_range, vel_mag, colorscale=colorscale)  # colorrange=(vmin, vmax))
@@ -252,6 +250,7 @@ function stream(vf::PlanarVelocityField; colorscale=identity)
         maxsteps=20000
     )
     Colorbar(fig[1, 2], hm, label="v")
+    colsize!(fig.layout, 1, Aspect(1, 1.0))
     fig
 end
 
